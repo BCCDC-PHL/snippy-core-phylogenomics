02a9f64ab5023e5e96c00733fd73269ced59868c
Dan Fornika <dfornika@gmail.com>
2024-04-10 11:56:26 -0700

    Adjust how snippy pipeline is run

--- .github/scripts/run_snippy_pipeline.sh
+++ .github/scripts/run_snippy_pipeline.sh
@@ -6,10 +6,11 @@ nextflow pull BCCDC-PHL/snippy-variants -r v0.1.0
 
 sed -i 's/cpus = 8/cpus = 4/g' $HOME/.nextflow/assets/BCCDC-PHL/snippy-variants/nextflow.config 
 
-nextflow run BCCDC-PHL/snippy-variants \
-	 -r v0.1.0 \
+nextflow run $HOME/.nextflow/assets/BCCDC-PHL/snippy-variants/main.nf \
+	 -c $HOME/.nextflow/assets/BCCDC-PHL/snippy-variants/nextflow.config \
 	 -profile conda \
 	 --cache ${HOME}/.conda/envs \
+	 --ref .github/data/assemblies/NC_000962.3.fa \
 	 --fastq_input .github/data/fastq \
 	 --outdir .github/data/test_output \
 	 -with-report .github/data/test_output/nextflow_report.html \
