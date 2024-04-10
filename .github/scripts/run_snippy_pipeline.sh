99819f7c9d852e7837ccb08ebece9297e41aeab7
Dan Fornika <dfornika@gmail.com>
2024-04-10 11:05:57 -0700

    use specific tag for snippy pipeline

--- .github/scripts/run_snippy_pipeline.sh
+++ .github/scripts/run_snippy_pipeline.sh
@@ -2,11 +2,12 @@
 
 set -eo pipefail
 
-nextflow pull BCCDC-PHL/snippy-variants
+nextflow pull BCCDC-PHL/snippy-variants -r v0.1.0
 
 sed -i 's/cpus = 8/cpus = 4/g' $HOME/.nextflow/assets/BCCDC-PHL/snippy-variants/nextflow.config 
 
 nextflow run BCCDC-PHL/snippy-variants \
+	 -r v0.1.0 \
 	 -profile conda \
 	 --cache ${HOME}/.conda/envs \
 	 --fastq_input .github/data/fastq \
