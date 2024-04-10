648bfdada9ab34e01dc13c7b7fcfe12500b7ec73
Dan Fornika <dfornika@gmail.com>
2024-04-10 14:40:32 -0700

    Fix ref filename

--- .github/scripts/run_snippy_core_pipeline.sh
+++ .github/scripts/run_snippy_core_pipeline.sh
@@ -9,7 +9,7 @@ outdir=".github/data/snippy-core-phylogenomics-output"
 nextflow run main.nf \
 	 -profile conda \
 	 --cache ${HOME}/.conda/envs \
-	 --ref .github/data/assemblies/NC_000913.3.fa \
+	 --ref .github/data/assemblies/NC_000962.3.fa \
 	 --snippy_dirs .github/data/snippy-variants-v0.1-output \
 	 --outdir ${outdir} \
 	 -with-report ${outdir}/nextflow_report.html \
