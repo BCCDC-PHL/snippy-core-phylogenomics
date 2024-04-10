c9ef807f47f51d53d4201f517ded39acc55b6332
Dan Fornika <dfornika@gmail.com>
2024-04-10 13:39:09 -0700

    Update pipeline scripts

--- .github/scripts/run_snippy_core_pipeline.sh
+++ .github/scripts/run_snippy_core_pipeline.sh
@@ -4,10 +4,13 @@ set -eo pipefail
 
 sed -i 's/cpus = 8/cpus = 4/g' nextflow.config 
 
+outdir = .github/data/snippy-core-phylogenomics-output
+
 nextflow run main.nf \
 	 -profile conda \
 	 --cache ${HOME}/.conda/envs \
-	 --fastq_input .github/data/fastq \
-	 --outdir .github/data/test_output \
-	 -with-report .github/data/test_output/nextflow_report.html \
- 	 -with-trace .github/data/test_output/nextflow_trace.tsv
+	 --ref .github/data/assemblies/NC_000913.3.fasta \
+	 --snippy_dirs .github/data/snippy-variants-v0.1-output \
+	 --outdir ${outdir} \
+	 -with-report ${outdir}/nextflow_report.html \
+ 	 -with-trace ${outdir}/nextflow_trace.tsv
--- .github/scripts/run_snippy_pipeline.sh
+++ .github/scripts/run_snippy_pipeline.sh
@@ -6,12 +6,14 @@ nextflow pull BCCDC-PHL/snippy-variants -r v0.1.0
 
 sed -i 's/cpus = 8/cpus = 4/g' $HOME/.nextflow/assets/BCCDC-PHL/snippy-variants/nextflow.config 
 
+outdir=".github/data/snippy-variants-v0.1-output"
+
 nextflow run $HOME/.nextflow/assets/BCCDC-PHL/snippy-variants/main.nf \
 	 -c $HOME/.nextflow/assets/BCCDC-PHL/snippy-variants/nextflow.config \
 	 -profile conda \
 	 --cache ${HOME}/.conda/envs \
 	 --ref .github/data/assemblies/NC_000962.3.fa \
 	 --fastq_input .github/data/fastq \
-	 --outdir .github/data/test_output \
-	 -with-report .github/data/test_output/nextflow_report.html \
- 	 -with-trace .github/data/test_output/nextflow_trace.tsv
+	 --outdir ${outdir} \
+	 -with-report ${outdir}/nextflow_report.html \
+ 	 -with-trace ${outdir}/nextflow_trace.tsv
